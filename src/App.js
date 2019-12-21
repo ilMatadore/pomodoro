12556f3f745ad075756febf2756d25c3df880e35
ilMatadore <restarter.dev@gmail.com>
2019-12-20 23:38:49 -0600

    starting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
+  render() {
+    return (
+      <div id="pomodoro">
+        <h2 id="title">Pomodoro Clock</h2>
+        <div id="break-control">
+          <div id="break-label">Break Length</div>
+          <button id="break-decrement">-</button>
+          <div id="break-length">5</div>
+          <button id="break-increment">+</button>
+        </div>
+        <div id="session-control">
+        <div id="session-label">Session Length</div>
+          <button id="session-decrement">-</button>
+          <div id="session-length">25</div>
+          <button id="session-increment">+</button>
+        </div>
+          <div id="timer-control">
+            <div id="timer-label">Session</div>
+            <div id="time-left">15:00</div>
+            <button id='start-stop'>Play/Pause</button>
+            <button id='reset'>Reset</button>
+          </div>
+      </div>
+    );
+}
+}
 export default App;
